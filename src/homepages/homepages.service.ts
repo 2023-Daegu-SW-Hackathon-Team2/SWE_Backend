30ac2409babb1137e69b8b0688a7d68f79dd1661
re1yun <re1yun@knu.ac.kr>
2023-11-12 07:06:24 +0900

    Add file upload functionality to HomepagesController and HomepagesService

--- src/homepages/homepages.service.ts
+++ src/homepages/homepages.service.ts
@@ -50,4 +50,61 @@ export class HomepagesService {
 
         return data as Homepages[];
     }
+
+    async uploadImage(file: Express.Multer.File): Promise<string> {
+        const filePath = `logos/${Date.now()}-${file.originalname}`;
+        await supabase.storage
+            .from('storage')
+            .upload(filePath, file.buffer, { contentType: file.mimetype });
+
+        const { data, error } = await supabase.storage
+            .from('storage')
+            .createSignedUrl(filePath, 60000);
+
+        await supabase
+            .from('Homepages')
+            .update({ logo: data['signedUrl'] })
+            .eq('id', 6);
+
+        return data['signedUrl'];
+    }
+
+    async uploadImages(files: Express.Multer.File[]): Promise<string[]> {
+        const filepath: string[] = [];
+        const uploadPromises = await files.map((file) => {
+            const filePath = `banners/${Date.now()}-${file.originalname}`;
+            return supabase.storage
+                .from('storage')
+                .upload(filePath, file.buffer, {
+                    contentType: file.mimetype,
+                })
+                .then(({ data, error }) => {
+                    if (error) {
+                        throw new Error(
+                            `Failed to upload image: ${file.originalname}`,
+                        );
+                    }
+                    filepath.push(filePath);
+                    return filepath;
+                });
+        });
+        await Promise.all(uploadPromises);
+        console.log('f', filepath);
+        const { data, error } = await supabase.storage
+            .from('storage')
+            .createSignedUrls(filepath, 60000);
+
+        // Supabase의 다른 테이블에 경로 저장
+        const result = [];
+        data.map((data) => {
+            console.log(data['signedUrl']);
+            result.push(data['signedUrl']);
+        });
+
+        await supabase
+            .from('Homepages')
+            .update({ banners: result })
+            .eq('id', 6);
+        return filepath;
+    }
 }
