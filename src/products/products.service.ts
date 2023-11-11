52ce183f146a047f5c0bd8d299252f6f2152ab07
Minj31 <momo1031@knu.ac.kr>
2023-11-11 18:41:00 +0900

    product_category

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -33,6 +33,20 @@ export class ProductsService {
         return data as Products[];
     }
 
+    async findCategory(category: number): Promise<Products[] | null> {
+        const { data, error } = await supabase
+            .from('Products')
+            .select('*')
+            .eq('category', category);
+
+        if (error) {
+            console.error('Error fetching data:', error);
+            return null;
+        }
+
+        return data as Products[];
+    }
+
     async update(id: number, products: Products): Promise<Products[] | null> {
         const { data, error } = await supabase
             .from('Products')
